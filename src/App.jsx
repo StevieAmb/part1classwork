7068f114136b227ff1477bfe88727544c3187ebc
Stevie Ambroise <s.ambroise@protonmail.com>
2024-11-02 19:24:04 -0600

    Edit the page to use props

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 
-const Hello = () => {
+const Hello = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Hello world</p>
+      Hello {props.name}, you are {props.age} years old
     </div>
   )
 }
 
 const App = () => {
+  const nimi = 'Pekka'
+  const ika = 10
+
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello />
+      <Hello name="Makia" age={10 + 26} />
+      <Hello name="Sienna" age={ika} />
+      <Hello name="Tyler" age={ika} />
+      <Hello name="Abem" age={ika} />
+      <Hello name="Alia" age={ika}/>
     </div>
   )
 }
