231d2511c046688e55544b7c0fa75062fb71b077
Stevie Ambroise <s.ambroise@protonmail.com>
2024-11-02 19:03:43 -0600

    Add code that renders and manipulates variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import './App.css'
 
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  console.log(now, a+b)
 
-export default App
+  return (
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+    </div>
+  )
+}
+
+export default App;
