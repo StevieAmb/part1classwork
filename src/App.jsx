a68dbeb0cd795fccccdf41054f75abe784360878
Stevie Ambroise <s.ambroise@protonmail.com>
2024-11-13 19:06:38 -0700

    Make the state object's properties individual object

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,9 @@ import { useState } from 'react'
 // }
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
   const handleLeftClick = () =>
     setClicks({ ...clicks, left: clicks.left + 1 })
